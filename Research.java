f20f766d4090ed1dbbc2b6c8f7cfaa1c3f9322b5
Rayast159 <tran-william@hotmail.fr>
2021-09-15 14:52:59 +0200

    Add research

---
+++ Research.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+import java.util.Vector;
+
+public class Research {
+    Vector<Integer> request = new Vector<Integer>();
+    HashMap<String, Vector<Double>> documentsVectors = new HashMap<String, Vector<Double>>();
+    public Research(HashMap<String, double[]> index) {
+
+    }
+}
